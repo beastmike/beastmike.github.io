b1f4ee2db712ec48fc2aa7e9af5b7c867a3d3ee8
Michael Biscante <michael.biscante@gmail.com>
2018-07-18 11:36:30 +0800

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+window.onload = (function () {
+    /**
+     * Particles.js
+     * Website: https://vincentgarreau.com/particles.js/
+     */
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles', '../particles.json', function () {
+        console.log('callback - particles-js config loaded');
+    });
+
+})();
+
+//smooth scroll on links
+(function () {
+    $(document).ready(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    })
+})();
