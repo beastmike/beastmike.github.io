96b450a25be5e0e3268e8c2ab78a35fcee124412
Miko <michael.biscante@gmail.com>
2018-07-18 19:20:16 +0800

    added particles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,40 @@
-window.onload = (function () {
+(function () {
+
     /**
-     * Particles.js
-     * Website: https://vincentgarreau.com/particles.js/
-     */
+         * Particles.js
+         * Website: https://vincentgarreau.com/particles.js/
+         */
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-    particlesJS.load('particles', '../particles.json', function () {
-        console.log('callback - particles-js config loaded');
+    particlesJS.load('particles', '../assets/particles.json');
+
+    $(document).ready(function () {
+        var innerHeight = window.innerHeight;
+        var centerScreen = innerHeight / 2;
+        var skillSectionTop = $('.skills h3').offset().top;
+        const $languages = $('.languages');
+        const $skillSection = $('.skills');
+        const $box = $('.box');
+        const $coverTitle = $('.cover-title');
+        const $arrow = $('.navigate-down a');
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > $coverTitle.offset().top) {
+                $arrow.fadeOut();
+            } else {
+                $arrow.fadeIn();
+            }
+            if ($(this).scrollTop() > skillSectionTop - centerScreen) {
+                $languages.addClass("languages-fade");
+                $box.addClass("box-rotate");
+            } else {
+                $languages.removeClass("languages-fade");
+                $box.removeClass("box-rotate");
+            }
+        });
     });
 
-})();
-
-//smooth scroll on links
-(function () {
+    // Smooth scroll 
     $(document).ready(function () {
-        $('a[href*=#]:not([href=#])').click(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -25,5 +46,6 @@ window.onload = (function () {
                 }
             }
         });
-    })
+    });
+
 })();
--- scripts.js
+++ scripts.js
@@ -1542,22 +1542,43 @@ window.particlesJS.load = function(tag_id, path_config_json, callback){
   xhr.send();
 
 };
-;window.onload = (function () {
+;(function () {
+
     /**
-     * Particles.js
-     * Website: https://vincentgarreau.com/particles.js/
-     */
+         * Particles.js
+         * Website: https://vincentgarreau.com/particles.js/
+         */
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-    particlesJS.load('particles', '../particles.json', function () {
-        console.log('callback - particles-js config loaded');
+    particlesJS.load('particles', '../assets/particles.json');
+
+    $(document).ready(function () {
+        var innerHeight = window.innerHeight;
+        var centerScreen = innerHeight / 2;
+        var skillSectionTop = $('.skills h3').offset().top;
+        const $languages = $('.languages');
+        const $skillSection = $('.skills');
+        const $box = $('.box');
+        const $coverTitle = $('.cover-title');
+        const $arrow = $('.navigate-down a');
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > $coverTitle.offset().top) {
+                $arrow.fadeOut();
+            } else {
+                $arrow.fadeIn();
+            }
+            if ($(this).scrollTop() > skillSectionTop - centerScreen) {
+                $languages.addClass("languages-fade");
+                $box.addClass("box-rotate");
+            } else {
+                $languages.removeClass("languages-fade");
+                $box.removeClass("box-rotate");
+            }
+        });
     });
 
-})();
-
-//smooth scroll on links
-(function () {
+    // Smooth scroll 
     $(document).ready(function () {
-        $('a[href*=#]:not([href=#])').click(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -1569,7 +1590,8 @@ window.particlesJS.load = function(tag_id, path_config_json, callback){
                 }
             }
         });
-    })
+    });
+
 })();
 ;
 //# sourceMappingURL=scripts.js.map
